13eabf221d786603bb42a330f23ef7dca34b22b3
CosmeNicolas <nicomas7590@gmail.com>
2024-05-19 21:53:13 -0300

    agregue un boton que al presionarlo nos lleva a la parte superior de la pagina principal

--- main.js
+++ main.js
@@ -29,3 +29,20 @@ function validarFormulario() {
   alert('Formulario enviado exitosamente.');
   return true;
 }
+
+window.onscroll = function() {
+  var backToTopBtn = document.getElementById('backToTopBtn');
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    backToTopBtn.style.display = 'block';
+  } else {
+    backToTopBtn.style.display = 'none';
+  }
+};
+
+// Función para desplazarse suavemente a la parte superior
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
